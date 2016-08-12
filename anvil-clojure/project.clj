13d0282a094d55b54574688e2acb6b02680c053e
John Lutteringer <jlutteringer@credera.com>
2016-08-12 16:28:03 -0500

    initial commit

---
+++ anvil-clojure/project.clj
@@ -0,0 +1,16 @@
+(defproject alloy/anvil-clojure "0.0.1-SNAPSHOT"
+  :plugins [[lein-modules "0.3.11"] [lein-cljsbuild "1.1.0"]]
+  :description "FIXME: write description"
+  :source-paths ["src/clj" "src/cljs" "src/cljc"]
+  :dependencies [
+                 [org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.198"]
+                 [com.rpl/specter "0.9.2"]]
+
+  :cljsbuild {:builds [{:source-paths ["src/cljc" "src/cljs"]
+                        :compiler     {
+                                       :output-to     "target/assets/js/alloy/core.js"
+                                       :optimizations :whitespace
+                                       :pretty-print  true
+                                       }}]}
+  :clean-targets ^{:protect false} [:target-path "target/assets/"])
